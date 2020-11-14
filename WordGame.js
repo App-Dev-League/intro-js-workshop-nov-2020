67e142196fc3c6e76115abc4d80ff166f7a17406
krinetic1234 <56781484+krinetic1234@users.noreply.github.com>
2020-11-13 22:57:16 -0800

    Update WordGame.js

--- WordGame.js
+++ WordGame.js
@@ -1,8 +1,12 @@
-let article = prompt("Enter an Article: ")
-let noun = prompt("Enter a Noun: ")
 let adj = prompt("Enter an Adjective: ")
+let noun = prompt("Enter a Noun: ")
 let verb = prompt("Enter a Verb: ")
 let article2 = prompt("Enter an Article: ")
 let adj2 = prompt("Enter another Adjective: ")
 let noun2 = prompt("Enter another Noun: ")
-console.log(article+ " "+ adj + " " + noun + " " + verb + " " + article2 + " " + adj2 + " " + noun2)
+
+//Option 1 (string concat)
+console.log("The" + " " + adj + " " + noun + " " + verb + " " + article2 + " " + adj2 + " " + noun2)
+
+//Option 2 (template literals)
+console.log(`The ${adj} ${noun} ${verb} ${article2} ${adj2} ${noun2}.`)
